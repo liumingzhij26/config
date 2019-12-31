aeffedbae43e6b779d7e1031efedd5db51f968e5
liumingzhi <liumingzhij26@qq.com>
2019-12-31 16:01:26 +0800

    feat: 多级文件夹加载

--- src/ConfigFactory.php
+++ src/ConfigFactory.php
@@ -27,7 +27,7 @@ class ConfigFactory
         $configPath = BASE_PATH . '/config/';
         $config = $this->readConfig($configPath . 'config.php');
         $serverConfig = $this->readConfig($configPath . 'server.php');
-        $autoloadConfig = $this->readPaths([BASE_PATH . '/config/autoload']);
+        $autoloadConfig = $this->readPathPhase([BASE_PATH . '/config/autoload']);
         $merged = array_merge_recursive(ProviderConfig::load(), $serverConfig, $config, ...$autoloadConfig);
         return new Config($merged);
     }
@@ -50,6 +50,35 @@ class ConfigFactory
             $configs[] = [
                 $file->getBasename('.php') => require $file->getRealPath(),
             ];
+        }
+        return $configs;
+    }
+
+    /**
+     * 读取多个目录下的 php 文件.
+     *
+     * api/wechat/english/Reading.php => dev/api/wechat/english/Reading.php
+     *
+     * @param array $paths
+     *
+     * @return array
+     */
+    private function readPathPhase(array $paths)
+    {
+        $configs = [];
+        $finder = new Finder();
+        $phase = env('PHASE'); //开发环境，如 dev
+        $finder->files()->in($paths)->exclude($phase)->name('*.php'); //如果指定了环境变量，就排除环境变量的配置扫描
+        foreach ($finder as $file) {
+            checkDirSameFile($file->getPath());
+            $filePath = $file->getRealPath();
+            if ($phase) {
+                $tmpPath = str_replace($file->getRelativePathname(), $phase . DIRECTORY_SEPARATOR . $file->getRelativePathname(), $file->getPathname());
+                if (file_exists($tmpPath) && is_readable($tmpPath)) {//如果开发环境中指定了文件路径，就替换读取文件
+                    $filePath = $tmpPath;
+                }
+            }
+            $configs[] = pathToArray(rtrim($file->getRelativePathname(), '.php'), require $filePath);
         }
         return $configs;
     }
--- src/Functions.php
+++ src/Functions.php
@@ -1,6 +1,7 @@
 <?php
 
 declare(strict_types=1);
+
 /**
  * This file is part of Hyperf.
  *
@@ -13,16 +14,56 @@ declare(strict_types=1);
 use Hyperf\Contract\ConfigInterface;
 use Hyperf\Utils\ApplicationContext;
 
-if (! function_exists('config')) {
+if (!function_exists('config')) {
     function config(string $key, $default = null)
     {
-        if (! ApplicationContext::hasContainer()) {
+        if (!ApplicationContext::hasContainer()) {
             throw new \RuntimeException('The application context lacks the container.');
         }
         $container = ApplicationContext::getContainer();
-        if (! $container->has(ConfigInterface::class)) {
+        if (!$container->has(ConfigInterface::class)) {
             throw new \RuntimeException('ConfigInterface is missing in container.');
         }
         return $container->get(ConfigInterface::class)->get($key, $default);
+    }
+}
+
+/**
+ * 文件路径转多维数据.
+ *
+ * a/b/c ['a']['b']['c']
+ *
+ * @param string $path
+ * @param array $value
+ *
+ * @return array
+ */
+if (!function_exists('pathToArray')) {
+    function pathToArray($path, array $value): array
+    {
+        $config = [];
+        $tmp = &$config;
+        foreach (explode(DIRECTORY_SEPARATOR, $path) as $key) {
+            $tmp[$key] = [];
+            $tmp = &$tmp[$key];
+        }
+        $tmp = $value;
+
+        return $config;
+    }
+}
+
+/**
+ * 判断文件夹是否有同名文件
+ *
+ * @param $dir
+ */
+if (!function_exists('checkDirSameFile')) {
+    function checkDirSameFile($dir)
+    {
+        $filename = sprintf($dir . '%s', '.php');
+        if (isFile($filename)) {
+            throw new \RuntimeException(sprintf('Cannot have the same name as the folder %s', $filename));
+        }
     }
 }
